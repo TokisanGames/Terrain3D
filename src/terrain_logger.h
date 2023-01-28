297d998aa7377f398c28c3d54569976451141a7d
Cory Petkovsek <632766+tinmanjuggernaut@users.noreply.github.com>
2023-01-28 23:17:00 +0700

    Fix logger error levels

--- src/terrain_logger.h
+++ src/terrain_logger.h
@@ -4,12 +4,18 @@
 
 #include "terrain.h"
 
+/**
+ * Prints warnings, errors, and regular messages to the console.
+ * Regular messages are filtered based on the user specified debug level.
+ * Warnings and errors always print except in release builds.
+ * DEBUG_CONT is for continuously called prints like inside snapping
+ */
 #define ERROR 0
-#define WARN 1
-#define INFO 2
-#define DEBUG 3
-#define DEBUG_CONT 4
-#define DEBUG_MAX 4
+#define WARN 99  // Higher than DEBUG_MAX so doesn't impact gdscript enum
+#define INFO 1
+#define DEBUG 2
+#define DEBUG_CONT 3
+#define DEBUG_MAX 3
 #define LOG(level, ...)                                                             \
 	if (level == ERROR)                                                             \
 		UtilityFunctions::push_error("Terrain3D::", __func__, ": ", __VA_ARGS__);   \
