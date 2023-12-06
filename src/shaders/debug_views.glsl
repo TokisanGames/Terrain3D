5279308e0ca75488745c40c6678f9e791e138d93
Cory Petkovsek <632766+TokisanGames@users.noreply.github.com>
2023-12-06 18:34:24 +0700

    Super privatize debug view vars, improve draw vertex grid

--- src/shaders/debug_views.glsl
+++ src/shaders/debug_views.glsl
@@ -3,12 +3,12 @@
 R"(
 //INSERT: DEBUG_CHECKERED
 	// Show a checkered grid
-	vec2 p = UV * 1.0; // scale
-	vec2 ddx = dFdx(p);
-	vec2 ddy = dFdy(p);
-	vec2 w = max(abs(ddx), abs(ddy)) + 0.01;
-	vec2 i = 2.0 * (abs(fract((p - 0.5 * w) / 2.0) - 0.5) - abs(fract((p + 0.5 * w) / 2.0) - 0.5)) / w;
-	ALBEDO = vec3((0.5 - 0.5 * i.x * i.y) * 0.2 + 0.2);
+	vec2 __p = UV * 1.0; // scale
+	vec2 __ddx = dFdx(__p);
+	vec2 __ddy = dFdy(__p);
+	vec2 __w = max(abs(__ddx), abs(__ddy)) + 0.01;
+	vec2 __i = 2.0 * (abs(fract((__p - 0.5 * __w) / 2.0) - 0.5) - abs(fract((__p + 0.5 * __w) / 2.0) - 0.5)) / __w;
+	ALBEDO = vec3((0.5 - 0.5 * __i.x * __i.y) * 0.2 + 0.2);
 	ROUGHNESS = 0.7;
 	NORMAL_MAP = vec3(0.5, 0.5, 1.0);
 
@@ -38,34 +38,36 @@ R"(
 
 //INSERT: DEBUG_CONTROL_TEXTURE
 	// Show control map texture selection
-	float ctrl_base = weight_inv * (
+	float __ctrl_base = weight_inv * (
 	float(mat[0].base) * weights.x +
 	float(mat[1].base) * weights.y +
 	float(mat[2].base) * weights.z +
 	float(mat[3].base) * weights.w )/96.;
-	float ctrl_over = weight_inv * (
+	float __ctrl_over = weight_inv * (
 	float(mat[0].over) * weights.x +
 	float(mat[1].over) * weights.y +
 	float(mat[2].over) * weights.z +
 	float(mat[3].over) * weights.w )/96.;
-	ALBEDO = vec3(ctrl_base, ctrl_over, 0.);
+	ALBEDO = vec3(__ctrl_base, __ctrl_over, 0.);
 	ROUGHNESS = 0.9;
 	NORMAL_MAP = vec3(0.5, 0.5, 1.0);
 
 //INSERT: DEBUG_CONTROL_BLEND
 	// Show control map blend values
-	float ctrl_blend = weight_inv * (
+	float __ctrl_blend = weight_inv * (
 	float(mat[0].blend) * weights.x +
 	float(mat[1].blend) * weights.y +
 	float(mat[2].blend) * weights.z +
 	float(mat[3].blend) * weights.w );
-	ALBEDO = vec3(ctrl_blend);
+	ALBEDO = vec3(__ctrl_blend);
 	ROUGHNESS = 1.0;
 	NORMAL_MAP = vec3(0.5, 0.5, 1.0);
 
 //INSERT: DEBUG_AUTOSHADER
-	float autoshader = float(control00 & 0x1u);
-	ALBEDO = vec3(autoshader);
+	ivec3 __ruv = get_region_uv(floor(UV));
+	uint __control = texelFetch(_control_maps, __ruv, 0).r;
+	float __autoshader = float( bool(__control & 0x1u) || __ruv.z<0 );
+	ALBEDO = vec3(__autoshader);
 
 //INSERT: DEBUG_TEXTURE_HEIGHT
 	// Show height textures
@@ -87,30 +89,32 @@ R"(
 
 //INSERT: DEBUG_VERTEX_GRID
 	// Show region and vertex grids
-	vec3 _pixel_pos = (INV_VIEW_MATRIX * vec4(VERTEX,1.0)).xyz;
-	float _camera_dist = length(v_camera_pos - _pixel_pos);
-	float _region_line = 0.5;		// Region line thickness
-	float _grid_line = 0.05;		// Vertex grid line thickness
-	float _grid_step = 1.0;			// Vertex grid size, 1.0 == integer units
-	float _vertex_size = 4.;		// Size of vertices
-	float _view_distance = 100.0;	// Visible distance of grid
+	vec3 __pixel_pos = (INV_VIEW_MATRIX * vec4(VERTEX,1.0)).xyz;
+	float __camera_dist = length(v_camera_pos - __pixel_pos);
+	float __region_line = 0.5;		// Region line thickness
+	float __grid_line = 0.05;		// Vertex grid line thickness
+	float __grid_step = 1.0;			// Vertex grid size, 1.0 == integer units
+	float __vertex_size = 4.;		// Size of vertices
+	float __view_distance = 300.0;	// Visible distance of grid
 	// Draw region grid
-	_region_line = .1*sqrt(_camera_dist);
-	if (mod(_pixel_pos.x + _region_line*.5, _region_size) <= _region_line || 
-		mod(_pixel_pos.z + _region_line*.5, _region_size) <= _region_line ) {
+	__region_line = .1*sqrt(__camera_dist);
+	if (mod(__pixel_pos.x + __region_line*.5, _region_size) <= __region_line || 
+		mod(__pixel_pos.z + __region_line*.5, _region_size) <= __region_line ) {
 		ALBEDO = vec3(1.);
 	}
-	if ( _camera_dist < _view_distance ) {
-		// Draw vertex grid
-		if ( mod(_pixel_pos.x + _grid_line*.5, _grid_step) < _grid_line || 
-		  	 mod(_pixel_pos.z + _grid_line*.5, _grid_step) < _grid_line ) { 
-				ALBEDO *= vec3(0.5);
-		}
-
-		// Draw Vertices
-		if ( mod(UV.x + _grid_line*_vertex_size*.5, _grid_step) < _grid_line*_vertex_size &&
-		  	 mod(UV.y + _grid_line*_vertex_size*.5, _grid_step) < _grid_line*_vertex_size ) { 
-				ALBEDO += vec3(0.15);
-		}
+	vec3 __vertex_mul = vec3(0.);
+	vec3 __vertex_add = vec3(0.);
+	float __distance_factor = clamp(1.-__camera_dist/__view_distance, 0., 1.);
+	// Draw vertex grid
+	if ( mod(__pixel_pos.x + __grid_line*.5, __grid_step) < __grid_line || 
+	  	 mod(__pixel_pos.z + __grid_line*.5, __grid_step) < __grid_line ) { 
+		__vertex_mul = vec3(0.5) * __distance_factor;
 	}
+	// Draw Vertices
+	if ( mod(UV.x + __grid_line*__vertex_size*.5, __grid_step) < __grid_line*__vertex_size &&
+	  	 mod(UV.y + __grid_line*__vertex_size*.5, __grid_step) < __grid_line*__vertex_size ) { 
+		__vertex_add = vec3(0.15) * __distance_factor;
+	}
+	ALBEDO = fma(ALBEDO, 1.-__vertex_mul, __vertex_add);
+
 )"
