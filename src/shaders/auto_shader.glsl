a9331da2cf31e9b26c8044f502732142da410b54
Cory Petkovsek <632766+TokisanGames@users.noreply.github.com>
2023-12-07 22:36:31 +0700

    Cleanup shaders

--- src/shaders/auto_shader.glsl
+++ src/shaders/auto_shader.glsl
@@ -8,7 +8,6 @@ uniform float auto_height_reduction : hint_range(0, 1) = 0.1;
 uniform int auto_base_texture : hint_range(0,31) = 0;
 uniform int auto_overlay_texture : hint_range(0,31) = 1;
 
-
 //INSERT: AUTO_SHADER_TEXTURE_ID
 	// Enable Autoshader if outside regions or painted in regions, otherwise manual painted
 	bool auto_shader = region<0 || bool(control & 0x1u);
@@ -19,20 +18,6 @@ uniform int auto_overlay_texture : hint_range(0,31) = 1;
 			- auto_height_reduction*.01*v_vertex.y // Reduce as vertices get higher
 			, 0., 1.) + 
 			 float(!auto_shader)*float(control >>14u & 0xFFu) * 0.003921568627450; // 1./255.0		
-
-//	) {
-//		out_mat.base = auto_base_texture;
-//		out_mat.over = auto_overlay_texture;
-//		out_mat.blend = clamp(
-//			dot(vec3(0., 1., 0.), normal * auto_slope*2. - (auto_slope*2.-1.)) 
-//			- auto_height_reduction*.01*v_vertex.y // Reduce as vertices get higher
-//			, 0., 1.);
-//	} else {
-//		// Manually painted
-//		out_mat.base = int(control >>27u & 0x1Fu);
-//		out_mat.over = int(control >> 22u & 0x1Fu);
-//		out_mat.blend = float(control >>14u & 0xFFu) * 0.003921568627450; // 1./255.0
-//	}
 
 //INSERT: TEXTURE_ID
 	out_mat.base = int(control >>27u & 0x1Fu);
