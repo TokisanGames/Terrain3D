770ab0578ee125bc16b6f95c6c6a016243b48680
jcoughenour <me@jacobcoughenour.com>
2023-12-11 22:03:39 -0500

    Texture filtering option now affects noise sampler

--- src/shaders/uniforms.glsl
+++ src/shaders/uniforms.glsl
@@ -5,9 +5,11 @@ R"(
 //INSERT: TEXTURE_SAMPLERS_LINEAR
 uniform sampler2DArray _texture_array_albedo : source_color, filter_linear_mipmap_anisotropic, repeat_enable;
 uniform sampler2DArray _texture_array_normal : hint_normal, filter_linear_mipmap_anisotropic, repeat_enable;
+uniform sampler2D noise_texture : source_color, filter_linear, repeat_enable;
 
 //INSERT: TEXTURE_SAMPLERS_NEAREST
 uniform sampler2DArray _texture_array_albedo : source_color, filter_nearest_mipmap_anisotropic, repeat_enable;
 uniform sampler2DArray _texture_array_normal : hint_normal, filter_nearest_mipmap_anisotropic, repeat_enable;
+uniform sampler2D noise_texture : source_color, filter_nearest, repeat_enable;
 
 )"
