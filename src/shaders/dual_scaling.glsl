eac0916fb6022d8b46bf2839a8f9973144625dc6
Cory Petkovsek <632766+TokisanGames@users.noreply.github.com>
2024-04-23 17:37:43 +0700

    Fix dual scaling & update docs

--- src/shaders/dual_scaling.glsl
+++ src/shaders/dual_scaling.glsl
@@ -8,10 +8,6 @@ uniform float dual_scale_reduction : hint_range(0.001,1) = 0.3;
 uniform float tri_scale_reduction : hint_range(0.001,1) = 0.3;
 uniform float dual_scale_far : hint_range(0,1000) = 170.0;
 uniform float dual_scale_near : hint_range(0,1000) = 100.0;
-varying float v_far_factor;
-
-//INSERT: DUAL_SCALING_VERTEX
-		v_far_factor = clamp(smoothstep(dual_scale_near, dual_scale_far, v_vertex_dist), 0.0, 1.0);
 
 //INSERT: DUAL_SCALING_BASE
 	// If dual scaling, apply to base texture
@@ -24,9 +20,11 @@ varying float v_far_factor;
 		albedo_far = texture(_texture_array_albedo, vec3(matUV*dual_scale_reduction, float(dual_scale_texture)));
 		normal_far = texture(_texture_array_normal, vec3(matUV*dual_scale_reduction, float(dual_scale_texture)));
 	}
+
+	float far_factor = clamp(smoothstep(dual_scale_near, dual_scale_far, length(v_vertex - v_camera_pos)), 0.0, 1.0);
 	if(out_mat.base == dual_scale_texture) {
-		albedo_ht = mix(albedo_ht, albedo_far, v_far_factor);
-		normal_rg = mix(normal_rg, normal_far, v_far_factor);
+		albedo_ht = mix(albedo_ht, albedo_far, far_factor);
+		normal_rg = mix(normal_rg, normal_far, far_factor);
 	}
 
 //INSERT: UNI_SCALING_BASE
@@ -36,8 +34,8 @@ varying float v_far_factor;
 //INSERT: DUAL_SCALING_OVERLAY
 		// If dual scaling, apply to overlay texture
 		if(out_mat.over == dual_scale_texture) {
-			albedo_ht2 = mix(albedo_ht2, albedo_far, v_far_factor);
-			normal_rg2 = mix(normal_rg2, normal_far, v_far_factor);
+			albedo_ht2 = mix(albedo_ht2, albedo_far, far_factor);
+			normal_rg2 = mix(normal_rg2, normal_far, far_factor);
 		}
 
 )"
