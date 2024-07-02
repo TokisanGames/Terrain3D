9685e3a6aa607518228f22a3191830d5a82ebd11
Tom Coxon <tom.c.coxon@gmail.com>
2024-07-02 20:21:05 +0100

    Add lift_floor and flatten_peaks terrain tool settings #275

--- src/terrain_3d_editor.cpp
+++ src/terrain_3d_editor.cpp
@@ -127,6 +127,8 @@ void Terrain3DEditor::_operate_map(Vector3 p_global_position, real_t p_camera_di
 	bool enable_scale = _brush_data["enable_scale"];
 	real_t scale = _brush_data["scale"];
 	real_t gamma = _brush_data["gamma"];
+	bool lift_floor = _brush_data["lift_floor"];
+	bool flatten_peaks = _brush_data["flatten_peaks"];
 
 	real_t randf = UtilityFunctions::randf();
 	real_t rot = randf * Math_PI * real_t(_brush_data["jitter"]);
@@ -223,16 +225,34 @@ void Terrain3DEditor::_operate_map(Vector3 p_global_position, real_t p_camera_di
 
 					switch (_operation) {
 						case ADD:
-							destf = srcf + (brush_alpha * strength * 10.f);
+							if (lift_floor && !std::isnan(p_global_position.y)) {
+								real_t brush_center_y = p_global_position.y + brush_alpha * strength;
+								destf = Math::clamp(brush_center_y, srcf, srcf + brush_alpha * strength);
+							} else {
+								destf = srcf + (brush_alpha * strength * 10.f);
+							}
 							break;
 						case SUBTRACT:
-							destf = srcf - (brush_alpha * strength * 10.f);
+							if (flatten_peaks && !std::isnan(p_global_position.y)) {
+								real_t brush_center_y = p_global_position.y - brush_alpha * strength;
+								destf = Math::clamp(brush_center_y, srcf - brush_alpha * strength, srcf);
+							} else {
+								destf = srcf - (brush_alpha * strength * 10.f);
+							}
 							break;
 						case MULTIPLY:
 							destf = srcf * (brush_alpha * strength * .01f + 1.0f);
+							if (lift_floor && !std::isnan(p_global_position.y)) {
+								real_t brush_center_y = p_global_position.y * (brush_alpha * strength * .01f + 1.0f);
+								destf = Math::clamp(brush_center_y, srcf, destf);
+							}
 							break;
 						case DIVIDE:
 							destf = srcf * (-brush_alpha * strength * .01f + 1.0f);
+							if (flatten_peaks && !std::isnan(p_global_position.y)) {
+								real_t brush_center_y = p_global_position.y * (-brush_alpha * strength * .01f + 1.0f);
+								destf = Math::clamp(brush_center_y, destf, srcf);
+							}
 							break;
 						case REPLACE:
 							destf = Math::lerp(srcf, height, brush_alpha * strength);
