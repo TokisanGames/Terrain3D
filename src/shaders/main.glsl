896d39d5b010c486ccc082f1fa0eab1899bac8f9
Xtarsia <69606701+Xtarsia@users.noreply.github.com>
2024-09-13 13:20:39 +0100

    Fix region borders on background mode NONE

--- src/shaders/main.glsl
+++ src/shaders/main.glsl
@@ -139,7 +139,7 @@ void vertex() {
 
 	// Show holes to all cameras except mouse camera (on exactly 1 layer)
 	if ( !(CAMERA_VISIBLE_LAYERS == _mouse_layer) && 
-			(hole || (_background_mode == 0u && v_region.z < 0)) ) {
+			(hole || (_background_mode == 0u && (get_region_uv(UV - _region_texel_size) & v_region).z < 0))) {
 		VERTEX.x = 0. / 0.;
 	} else {		
 		// Set final vertex height & calculate vertex normals. 3 lookups.
