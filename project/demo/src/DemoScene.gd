c72057f048d38376a77920a5c8eb3b6de4bf7d3e
Cory Petkovsek <632766+TokisanGames@users.noreply.github.com>
2024-10-25 21:52:06 +0700

    Update godot-cpp, and minimum version to 4.3

--- project/demo/src/DemoScene.gd
+++ project/demo/src/DemoScene.gd
@@ -5,4 +5,3 @@ extends Node
 func _ready():
 	if not Engine.is_editor_hint() and has_node("UI"):
 		$UI.player = $Player
-
--- project/demo/src/UI.gd
+++ project/demo/src/UI.gd
@@ -62,4 +62,3 @@ func toggle_fullscreen() -> void:
 		DisplayServer.window_set_size(Vector2(1280, 720))
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
-
