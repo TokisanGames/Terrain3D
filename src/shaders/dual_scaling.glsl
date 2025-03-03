d4830fd7fc88880bd1a66bafbf360cd1baa13087
Xtarsia <69606701+Xtarsia@users.noreply.github.com>
2025-03-03 14:22:40 +0000

    another fix

--- src/shaders/dual_scaling.glsl
+++ src/shaders/dual_scaling.glsl
@@ -65,7 +65,7 @@ uniform float dual_scale_near : hint_range(0,1000) = 100.0;
 		normal_rg = textureGrad(_texture_array_normal, vec3(matUV, float(out_mat.base)), dd1.xy, dd1.zw);
 
 		// Unpack & rotate base normal for blending
-		normal_rg.xz = unpack_normal(normal_rg).xz;
+		normal_rg.xyz = unpack_normal(normal_rg).xyz;
 		normal_rg.xz = rotate_plane(normal_rg.xz, -normal_angle);
 	}
 
