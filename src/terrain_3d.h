2c81f97456d6221e8f97f0a1c5cfc73b95c1ccf6
Cory Petkovsek <632766+TokisanGames@users.noreply.github.com>
2025-03-29 17:18:38 +0700

    ﻿Bump version to 1.0.1

--- src/terrain_3d.h
+++ src/terrain_3d.h
@@ -47,7 +47,7 @@ public: // Constants
 	};
 
 private:
-	String _version = "1.0.0";
+	String _version = "1.0.1";
 	String _data_directory;
 	bool _is_inside_world = false;
 	bool _initialized = false;
