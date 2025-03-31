565a10957e5b2973b181595af1c97ac52c95aefa
Cory Petkovsek <632766+TokisanGames@users.noreply.github.com>
2025-03-31 14:38:00 +0700

    Fix #661 - crash from unchecked array index

--- src/terrain_3d_assets.h
+++ src/terrain_3d_assets.h
@@ -68,7 +68,7 @@ public:
 	void destroy();
 
 	void set_texture(const int p_id, const Ref<Terrain3DTextureAsset> &p_texture);
-	Ref<Terrain3DTextureAsset> get_texture(const int p_id) const { return _texture_list[p_id]; }
+	Ref<Terrain3DTextureAsset> get_texture(const int p_id) const;
 	void set_texture_list(const TypedArray<Terrain3DTextureAsset> &p_texture_list);
 	TypedArray<Terrain3DTextureAsset> get_texture_list() const { return _texture_list; }
 	int get_texture_count() const { return _texture_list.size(); }
@@ -98,5 +98,13 @@ protected:
 };
 
 VARIANT_ENUM_CAST(Terrain3DAssets::AssetType);
+
+inline Ref<Terrain3DTextureAsset> Terrain3DAssets::get_texture(const int p_id) const {
+	if (p_id >= 0 && p_id < _texture_list.size()) {
+		return _texture_list[p_id];
+	} else {
+		return Ref<Terrain3DTextureAsset>();
+	}
+}
 
 #endif // TERRAIN3D_ASSETS_CLASS_H
