b6f05ba7147daef94df23e9d1bf638caea5ea14e
aidandavey <71520018+aidandavey@users.noreply.github.com>
2025-05-28 20:04:10 +0100

    Reset demo project

--- project/demo/src/DemoScene.gd
+++ project/demo/src/DemoScene.gd
@@ -2,17 +2,6 @@
 extends Node
 
 @onready var terrain: Terrain3D = find_child("Terrain3D")
-@export var nb_test_instance_rows:int = 100:
-	set(value):
-		nb_test_instance_rows = value
-		if Engine.is_editor_hint():		
-			spawn_instances(nb_test_instance_rows, test_instance_pitch)
-			
-@export var test_instance_pitch:float = 1.0:
-	set(value):
-		test_instance_pitch = value
-		if Engine.is_editor_hint():
-			spawn_instances(nb_test_instance_rows, test_instance_pitch)
 
 
 func _ready():
@@ -30,19 +19,5 @@ func _ready():
 			sky3d.owner = self
 			sky3d.current_time = 10
 			sky3d.enable_editor_time = false
+			
 		
-	spawn_instances(nb_test_instance_rows, test_instance_pitch)
-		
-func spawn_instances(width: int = 10, pitch:float = 20.0):
-	if nb_test_instance_rows == 0:
-		return
-	terrain.instancer.clear_by_mesh(2)
-	var xforms: Array[Transform3D]
-	
-	var step: int = 1
-	for x in range(0, width, step):
-		for z in range(0, width, step):
-			var pos: Vector3 = $Player.global_position + Vector3(x*pitch, 0, z*pitch) - Vector3(width, 0, width) * .5
-			pos.y = terrain.data.get_height(pos)
-			xforms.push_back(Transform3D(Basis(), pos))
-	terrain.instancer.add_transforms(2, xforms)
