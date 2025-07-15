TokisanGames/Terrain3D

commit ec8851bff5547391810e2b057cc6b9755152043b

Add custom collision and clipmap targets
Update terrain_3d.cpp

Update terrain_3d.cpp

Update terrain_3d.cpp

src/terrain_3d.cpp
@@ -113,28 +113,25 @@ void Terrain3D::__physics_process(const double p_delta) {
 	if (!_initialized)
 		return;
 
-	// If the game/editor camera is not set, find it
-	if (!is_instance_valid(_camera_instance_id, _camera)) {
+	if (!_camera.is_valid()) {
 		LOG(DEBUG, "Camera is null, getting the current one");
 		_grab_camera();
 	}
 
-	// If camera has moved enough, re-center the terrain on it.
-	if (is_instance_valid(_camera_instance_id) && _camera->is_inside_tree()) {
-		Vector3 cam_pos = _camera->get_global_position();
-		Vector2 cam_pos_2d = Vector2(cam_pos.x, cam_pos.z);
-		RS->material_set_param(_material->get_material_rid(), "_camera_pos", cam_pos);
-		if (_camera_last_position.distance_to(cam_pos_2d) > 0.2f) {
-			if (_mesher) {
-				_mesher->snap(cam_pos);
-			}
-			_snapped_position = (cam_pos / _vertex_spacing).floor() * _vertex_spacing;
-			_camera_last_position = cam_pos_2d;
-			if (_collision && _collision->is_dynamic_mode()) {
-				_collision->update();
-			}
+	// If clipmap target has moved enough, re-center the terrain on it.
+	Vector3 target_pos = get_clipmap_target_position();
+	Vector2 target_pos_2d = Vector2(target_pos.x, target_pos.z);
+	RS->material_set_param(_material->get_material_rid(), "_camera_pos", target_pos);
+	if (_camera_last_position.distance_to(target_pos_2d) > 0.2f) {
+		if (_mesher) {
+			_mesher->snap(target_pos);
 		}
+		_snapped_position = (target_pos / _vertex_spacing).floor() * _vertex_spacing;
+		_camera_last_position = target_pos_2d;
 	}
+
+	// For collision, distance moved and re-centering is handled internally.
+	_collision->update();
 }
 
 /**
@@ -143,18 +140,15 @@ void Terrain3D::__physics_process(const double p_delta) {
  */
 void Terrain3D::_grab_camera() {
 	if (IS_EDITOR) {
-		_camera = EditorInterface::get_singleton()->get_editor_viewport_3d(0)->get_camera_3d();
-		LOG(DEBUG, "Grabbing the first editor viewport camera: ", _camera);
+		_camera.set_target(EditorInterface::get_singleton()->get_editor_viewport_3d(0)->get_camera_3d());
+		LOG(DEBUG, "Grabbing the first editor viewport camera: ", _camera.get_target());
 	} else {
-		_camera = get_viewport()->get_camera_3d();
-		LOG(DEBUG, "Grabbing the in-game viewport camera: ", _camera);
+		_camera.set_target(get_viewport()->get_camera_3d());
+		LOG(DEBUG, "Grabbing the in-game viewport camera: ", _camera.get_target());
 	}
-	if (_camera) {
-		_camera_instance_id = _camera->get_instance_id();
-	} else {
-		_camera_instance_id = 0;
+	if (!_camera.is_valid() && !_clipmap_target_override.is_valid()) {
 		set_physics_process(false); // disable snapping
-		LOG(ERROR, "Cannot find the active camera. Set it manually with Terrain3D.set_camera(). Stopping _physics_process()");
+		LOG(ERROR, "Cannot find clipmap target or the active camera. Set it manually with Terrain3D.set_camera(). Stopping _physics_process()");
 	}
 }
 
@@ -480,19 +474,52 @@ void Terrain3D::set_plugin(EditorPlugin *p_plugin) {
 }
 
 void Terrain3D::set_camera(Camera3D *p_camera) {
-	if (_camera != p_camera) {
-		_camera = p_camera;
-		if (!p_camera) {
-			LOG(DEBUG, "Received null camera. Calling _grab_camera()");
-			_grab_camera();
-		} else {
-			_camera = p_camera;
-			_camera_instance_id = _camera->get_instance_id();
-			LOG(DEBUG, "Setting camera: ", _camera);
-			_initialize();
-			set_physics_process(true); // enable snapping
-		}
+	LOG(INFO, "Setting camera");
+	_camera.set_target(p_camera);
+}
+
+Node3D *Terrain3D::get_collision_target_override() {
+	return _collision_target_override.get_target();
+}
+
+void Terrain3D::set_collision_target_override(Node3D *p_node) {
+	LOG(INFO, "Setting collision target override");
+	_collision_target_override.set_target(p_node);
+}
+
+Vector3 Terrain3D::get_collision_target_position() {
+	if (_collision_target_override.is_valid()) {
+		return _collision_target_override.get_global_position();
 	}
+	if (_camera.is_valid()) {
+		return _camera.get_global_position();
+	}
+	if (_clipmap_target_override.is_valid()) {
+		return _clipmap_target_override.get_global_position();
+	}
+
+	return Vector3(NAN, NAN, NAN);
+}
+
+Node3D *Terrain3D::get_clipmap_target_override() {
+	return _clipmap_target_override.get_target();
+}
+
+void Terrain3D::set_clipmap_target_override(Node3D *p_node) {
+	LOG(INFO, "Setting clipmap target override");
+	_clipmap_target_override.set_target(p_node);
+}
+
+Vector3 Terrain3D::get_clipmap_target_position() {
+	if (_clipmap_target_override.is_valid()) {
+		return _clipmap_target_override.get_global_position();
+	}
+
+	if (_camera.is_valid()) {
+		return _camera.get_global_position();
+	}
+
+	return Vector3(NAN, NAN, NAN);
 }
 
 void Terrain3D::set_region_size(const RegionSize p_size) {
@@ -665,7 +692,8 @@ void Terrain3D::set_cull_margin(const real_t p_margin) {
  * Returns Vec3(NAN) on error or vec3(3.402823466e+38F) on no intersection. Test w/ if (var.x < 3.4e38)
  */
 Vector3 Terrain3D::get_intersection(const Vector3 &p_src_pos, const Vector3 &p_direction, const bool p_gpu_mode) {
-	if (!is_instance_valid(_camera_instance_id)) {
+	// Why does this need a camera...?
+	if (!_camera.is_valid()) {
 		LOG(ERROR, "Invalid camera");
 		return Vector3(NAN, NAN, NAN);
 	}
@@ -1064,6 +1092,8 @@ void Terrain3D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_collision_priority"), &Terrain3D::get_collision_priority);
 	ClassDB::bind_method(D_METHOD("set_physics_material", "material"), &Terrain3D::set_physics_material);
 	ClassDB::bind_method(D_METHOD("get_physics_material"), &Terrain3D::get_physics_material);
+	ClassDB::bind_method(D_METHOD("set_collision_target_override", "node"), &Terrain3D::set_collision_target_override);
+	ClassDB::bind_method(D_METHOD("get_collision_target_override"), &Terrain3D::get_collision_target_override);
 
 	// Meshes
 	ClassDB::bind_method(D_METHOD("set_mesh_lods", "count"), &Terrain3D::set_mesh_lods);
@@ -1073,6 +1103,8 @@ void Terrain3D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_vertex_spacing", "scale"), &Terrain3D::set_vertex_spacing);
 	ClassDB::bind_method(D_METHOD("get_vertex_spacing"), &Terrain3D::get_vertex_spacing);
 	ClassDB::bind_method(D_METHOD("get_snapped_position"), &Terrain3D::get_snapped_position);
+	ClassDB::bind_method(D_METHOD("set_clipmap_target_override", "node"), &Terrain3D::set_clipmap_target_override);
+	ClassDB::bind_method(D_METHOD("get_clipmap_target_override"), &Terrain3D::get_clipmap_target_override);
 
 	// Rendering
 	ClassDB::bind_method(D_METHOD("set_render_layers", "layers"), &Terrain3D::set_render_layers);
@@ -1157,6 +1189,7 @@ void Terrain3D::_bind_methods() {
 
 	ADD_GROUP("Collision", "");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "collision_mode", PROPERTY_HINT_ENUM, "Disabled,Dynamic / Game,Dynamic / Editor,Full / Game,Full / Editor"), "set_collision_mode", "get_collision_mode");
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "collision_target_override", PROPERTY_HINT_NODE_TYPE, "Node3D"), "set_collision_target_override", "get_collision_target_override");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "collision_shape_size", PROPERTY_HINT_RANGE, "8,64,8"), "set_collision_shape_size", "get_collision_shape_size");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "collision_radius", PROPERTY_HINT_RANGE, "16,256,16"), "set_collision_radius", "get_collision_radius");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "collision_layer", PROPERTY_HINT_LAYERS_3D_PHYSICS), "set_collision_layer", "get_collision_layer");
@@ -1165,6 +1198,7 @@ void Terrain3D::_bind_methods() {
 	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "physics_material", PROPERTY_HINT_RESOURCE_TYPE, "PhysicsMaterial"), "set_physics_material", "get_physics_material");
 
 	ADD_GROUP("Mesh", "");
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "clipmap_target_override", PROPERTY_HINT_NODE_TYPE, "Node3D"), "set_clipmap_target_override", "get_clipmap_target_override");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "mesh_lods", PROPERTY_HINT_RANGE, "1,10,1"), "set_mesh_lods", "get_mesh_lods");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "mesh_size", PROPERTY_HINT_RANGE, "8,64,2"), "set_mesh_size", "get_mesh_size");
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "vertex_spacing", PROPERTY_HINT_RANGE, "0.25,10.0,0.05,or_greater"), "set_vertex_spacing", "get_vertex_spacing");

src/terrain_3d_collision.cpp
@@ -296,7 +296,7 @@ void Terrain3DCollision::update(const bool p_rebuild) {
 
 	if (is_dynamic_mode()) {
 		// Snap descaled position to a _shape_size grid (eg. multiples of 16)
-		Vector2i snapped_pos = _snap_to_grid(_terrain->get_snapped_position() / spacing);
+		Vector2i snapped_pos = _snap_to_grid(_terrain->get_collision_target_position() / spacing);
 		LOG(EXTREME, "Updating collision at ", snapped_pos);
 
 		// Skip if location hasn't moved to next step

src/terrain_3d.h
@@ -23,6 +23,58 @@
 
 using namespace godot;
 
+// Does this struct belong here?
+struct TargetNode3D {
+
+private:
+	uint64_t _instance_id = 0;
+	Node3D *_target = nullptr;
+
+public:
+	void set_target(Node3D *p_node_3d) {
+		if (p_node_3d) {
+			_target = p_node_3d;
+			_instance_id = p_node_3d->get_instance_id();
+		} else {
+			_target = nullptr;
+			_instance_id = 0;
+		}
+	}
+
+	Node3D *get_target() {
+		return _target;
+	}
+
+	Vector3 get_global_position() {
+		if (is_inside_tree()) {
+			return _target->get_global_position();
+		}
+		return Vector3(NAN, NAN, NAN);
+	}
+	
+	// UtilityFunctions::is_instance_valid() is faulty and shouldn't be used.
+	// See https://github.com/godotengine/godot-cpp/issues/1390#issuecomment-1937570699
+	bool is_valid() {
+		Object *obj = ObjectDB::get_instance(_instance_id);
+		if (_target) {
+			return _instance_id > 0 && _target == obj;
+		} else {
+			return _instance_id > 0 && obj;
+		}
+	}
+
+	bool is_null() { 
+		return !is_valid(); 
+	}
+
+	bool is_inside_tree() {
+		if (is_valid()) {
+			return _target->is_inside_tree();
+		}
+		return false;
+	}
+};
+
 class Terrain3D : public Node3D {
 	GDCLASS(Terrain3D, Node3D);
 	CLASS_NAME();
@@ -62,11 +114,13 @@ private:
 	Terrain3DMesher *_mesher = nullptr;
 	Terrain3DEditor *_editor = nullptr;
 	EditorPlugin *_plugin = nullptr;
-	// Current editor or gameplay camera we are centering the terrain on.
-	Camera3D *_camera = nullptr;
-	uint64_t _camera_instance_id = 0;
+
 	// X,Z Position of the camera during the previous snapping. Set to max real_t value to force a snap update.
 	Vector2 _camera_last_position = V2_MAX;
+
+	TargetNode3D _camera; // Fallback target for clipmap and collision
+	TargetNode3D _collision_target_override;
+	TargetNode3D _clipmap_target_override;
 
 	// Regions
 	RegionSize _region_size = SIZE_256;
@@ -146,7 +200,15 @@ public:
 	void set_plugin(EditorPlugin *p_plugin);
 	EditorPlugin *get_plugin() const { return _plugin; }
 	void set_camera(Camera3D *p_camera);
-	Camera3D *get_camera() const { return _camera; }
+	Node3D *get_camera() { return _camera.get_target(); }
+
+	Node3D *get_collision_target_override();
+	void set_collision_target_override(Node3D *p_node);
+	Vector3 get_collision_target_position();
+
+	Node3D *get_clipmap_target_override();
+	void set_clipmap_target_override(Node3D *p_node);
+	Vector3 get_clipmap_target_position();
 
 	// Regions
 	void set_region_size(const RegionSize p_size);

src/terrain_3d_util.h
@@ -267,18 +267,6 @@ _FORCE_INLINE_ bool remove_from_tree(Node *p_node) {
 	return false;
 }
 
-// UtilityFunctions::is_instance_valid() is faulty and shouldn't be used.
-// Use this version instead on objects that might be freed by the user.
-// See https://github.com/godotengine/godot-cpp/issues/1390#issuecomment-1937570699
-_FORCE_INLINE_ bool is_instance_valid(const uint64_t p_instance_id, Object *p_object = nullptr) {
-	Object *obj = ObjectDB::get_instance(p_instance_id);
-	if (p_object) {
-		return p_instance_id > 0 && p_object == obj;
-	} else {
-		return p_instance_id > 0 && obj;
-	}
-}
-
 _FORCE_INLINE_ String ptr_to_str(const void *p_ptr) {
 	return "0x" + String::num_uint64(uint64_t(p_ptr), 16, true);
 }

commit 0edd348fe41644b92d2b9f677a036791e3a2454f

Fix Full collision mode runaway crash, regression from ec8851b; Fix slow debug shapes

src/terrain_3d.cpp
@@ -119,7 +119,7 @@ void Terrain3D::__physics_process(const double p_delta) {
 	if (_mesher) {
 		_mesher->snap();
 	}
-	if (_collision) {
+	if (_collision && _collision->is_dynamic_mode()) {
 		_collision->update();
 	}
 }

src/terrain_3d_collision.cpp
@@ -264,6 +264,7 @@ void Terrain3DCollision::build() {
 			col_shape->set_name("CollisionShape3D");
 			col_shape->set_disabled(true);
 			col_shape->set_visible(true);
+			col_shape->set_enable_debug_fill(false);
 			Ref<HeightMapShape3D> hshape;
 			hshape.instantiate();
 			hshape->set_map_width(hshape_size);