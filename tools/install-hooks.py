61538d012d2e03dacd430997d671135fb0c2d7f8
Jackerty <contact.jackerty@gmail.com>
2025-11-04 19:18:55 +0200

    Installing hook did not set execution bit on Linux.
    The precommit install script does not set execution bit. Linux user has to manually
    flip that. Commit adds this part to the install script.
    
    Files ".git/hooks/pre-commit" and ".git/hooks/pre-commit-clang-format" are set to "-rwxr--r--" a.k.a
    owning user has read write execute rights; group with owning user and others have only read rights.
    
    Platform check is made so that no weird windows problems occur. Mac not tested.

--- tools/install-hooks.py
+++ tools/install-hooks.py
@@ -1,4 +1,5 @@
 import os
+import stat
 import sys
 import shutil
 
@@ -11,5 +12,11 @@ if not os.path.exists(dist_dir):
 
 for file in os.listdir(src_dir):
     shutil.copy(os.path.join(src_dir, file), dist_dir)
+
+# Unix user needs the execute bit set.
+if os.name == "posix":
+    mode = stat.S_IRWXU | stat.S_IRGRP | stat.S_IROTH;
+    os.chmod(os.path.join(dist_dir, "pre-commit"), mode);
+    os.chmod(os.path.join(dist_dir, "pre-commit-clang-format"), mode);
 
 print("Copied tools/hooks/* to .git/hooks")
