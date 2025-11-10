668f5d395d428f9075918e1e30af437388eee4e3
Xtarsia <69606701+Xtarsia@users.noreply.github.com>
2025-11-10 10:17:51 +0000

    count generated array length instead of asset array

--- src/generated_texture.cpp
+++ src/generated_texture.cpp
@@ -21,6 +21,7 @@ void GeneratedTexture::clear() {
 	}
 	_rid = RID();
 	_dirty = true;
+	_size = 0;
 }
 
 RID GeneratedTexture::create(const TypedArray<Image> &p_layers) {
@@ -34,6 +35,7 @@ RID GeneratedTexture::create(const TypedArray<Image> &p_layers) {
 		}
 		_rid = RS->texture_2d_layered_create(p_layers, RenderingServer::TEXTURE_LAYERED_2D_ARRAY);
 		_dirty = false;
+		_size = p_layers.size();
 	} else {
 		clear();
 	}
