800a95b1312e908fad256574da0a80dec2cd4438
aidandavey <71520018+aidandavey@users.noreply.github.com>
2025-12-14 21:43:13 +0000

    Improve instance collision (+3 squashed commit)
    Squashed commit:
    
    [86d0e7e] Separate instance collision modes and other improvements
    
    [d47e74c] Do not clear debug mesh array before queued updates are processed
    
    [f4e5ab9] Fix distance check XOR error, don't pass native types as pointers and pass in radius as a param

--- src/terrain_3d_collision.h
+++ src/terrain_3d_collision.h
@@ -25,12 +25,17 @@ public: // Constants
 		FULL_GAME,
 		FULL_EDITOR,
 	};
+	enum InstanceCollisionMode {
+		INSTANCE_COLLISION_DISABLED,
+		INSTANCE_COLLISION_DYNAMIC_GAME,
+		INSTANCE_COLLISION_DYNAMIC_EDITOR,
+	};
 
 private:
 	Terrain3D *_terrain = nullptr;
 
 	// Public settings
-	CollisionMode _mode = DYNAMIC_GAME;
+	CollisionMode _mode = CollisionMode::DYNAMIC_GAME;
 	uint16_t _shape_size = 16;
 	uint16_t _radius = 64;
 	uint32_t _layer = 1;
@@ -47,12 +52,17 @@ private:
 	Vector2i _last_snapped_pos = V2I_MAX;
 
 	// Instance collision data
+	InstanceCollisionMode _instance_collision_mode = InstanceCollisionMode::INSTANCE_COLLISION_DYNAMIC_GAME;
+	Vector2i _last_snapped_pos_instance_collision = V2I_MAX;
+	real_t _instance_collision_radius = 64.f;
+	bool _instance_collision_is_dirty = false;
+
 	RID _instance_static_body_rid;
 
 	// Stored as {PS_rid:RID} -> RS_rid:RID
 	Dictionary _shape_debug_mesh_pairs = {};
 
-	// Stored as {cell_loc:v3} -> {mesh_asset_id:int} -> [instances [shapes [RID]]]
+	// Stored as {cell_loc:v2} -> {mesh_asset_id:int} -> [instances [shapes [RID]]]
 	Dictionary _active_instance_cells = {};
 
 	// Stored as {rid:RID} -> {PS_body_to_shape_index:int}
@@ -85,15 +95,13 @@ private:
 
 	void _reload_physics_material();
 
-	TypedArray<Vector3> _get_instance_cells_to_build(const Vector2i &p_snapped_pos, const int &p_region_size, const int &p_cell_size, const real_t &p_vertex_spacing);
-	Dictionary _get_recyclable_instances(const Vector2i &p_snapped_pos, const real_t &p_radius);
-	Dictionary _get_instance_build_data(const TypedArray<Vector3> &p_instance_cells_to_build, const int &p_region_size, const real_t &p_vertex_spacing);
+	TypedArray<Vector2> _get_instance_cells_to_build(const Vector2i &p_snapped_pos, const real_t p_radius, const int p_region_size, const int p_cell_size, const real_t p_vertex_spacing);
+	Dictionary _get_recyclable_instances(const Vector2i &p_snapped_pos, const real_t p_radius, const int p_cell_size, const real_t p_vertex_spacing);
+	Dictionary _get_instance_build_data(const TypedArray<Vector2> &p_instance_cells_to_build, const int p_region_size, const int p_cell_size, const real_t p_vertex_spacing);
 	Dictionary _get_unused_instance_shapes(const Dictionary &p_mesh_instance_build_data, Dictionary &p_recyclable_mesh_instance_shapes);
 
 	void _destroy_remaining_instance_shapes(Dictionary &p_unused_instance_shapes);
 	void _generate_instances(const Dictionary &p_instance_build_data, Dictionary &p_recyclable_instances, Dictionary &p_unused_instance_shapes);
-	void _update_instance_collision();
-	void _destroy_instance_collision();
 	void _create_debug_mesh_instance(const RID &p_shape_rid, const Transform3D &p_xform, const Ref<ArrayMesh> &p_debug_mesh);
 	void _update_debug_mesh_instance(const RID &p_shape_rid, const Transform3D &p_xform, const Ref<ArrayMesh> &p_debug_mesh);
 	void _destroy_debug_mesh_instance(const RID &p_shape_rid);
@@ -118,6 +126,17 @@ public:
 	bool is_editor_mode() const { return _mode == DYNAMIC_EDITOR || _mode == FULL_EDITOR; }
 	bool is_dynamic_mode() const { return _mode == DYNAMIC_GAME || _mode == DYNAMIC_EDITOR; }
 
+	void update_instance_collision();
+	void destroy_instance_collision();
+	void set_instance_collision_mode(const InstanceCollisionMode p_mode);
+	InstanceCollisionMode get_instance_collision_mode() const { return _instance_collision_mode; }
+	bool is_instance_collision_enabled() const { return _instance_collision_mode > InstanceCollisionMode::INSTANCE_COLLISION_DISABLED; }
+	bool is_instance_collision_editor_mode() const { return _instance_collision_mode == InstanceCollisionMode::INSTANCE_COLLISION_DYNAMIC_EDITOR; }
+	bool is_instance_collision_dynamic_mode() const { return _instance_collision_mode == InstanceCollisionMode::INSTANCE_COLLISION_DYNAMIC_GAME || _instance_collision_mode == InstanceCollisionMode::INSTANCE_COLLISION_DYNAMIC_EDITOR; }
+	void set_instance_collision_radius(const real_t p_radius);
+	real_t get_instance_collision_radius() const { return _instance_collision_radius; }
+	void set_instance_collision_dirty(const bool p_dirty) { _instance_collision_is_dirty = p_dirty; }
+
 	void set_shape_size(const uint16_t p_size);
 	uint16_t get_shape_size() const { return _shape_size; }
 	void set_radius(const uint16_t p_radius);
@@ -138,6 +157,9 @@ protected:
 
 using CollisionMode = Terrain3DCollision::CollisionMode;
 VARIANT_ENUM_CAST(Terrain3DCollision::CollisionMode);
+
+using InstanceCollisionMode = Terrain3DCollision::InstanceCollisionMode;
+VARIANT_ENUM_CAST(Terrain3DCollision::InstanceCollisionMode);
 
 inline Vector2i Terrain3DCollision::_snap_to_grid(const Vector2i &p_pos) const {
 	return Vector2i(int_round_mult(p_pos.x, int32_t(_shape_size)),
